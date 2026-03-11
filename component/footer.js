c52182024d7f2cda69fc16a5dcb14cb46facd1a6
AbdullahGhassanRR <aghassanrr@gmail.com>
2026-03-11 11:30:51 +0700

    11.30 am sebelum presentasi

--- component/footer.js
+++ component/footer.js
@@ -4,21 +4,23 @@ const html_footer =   `
 
             <div class="footer-contact">
                 <h2 class="footer-contact-header">Contact Us</h2>
-                <p>alsalcunpad@gmail.com</p>
+                <p>unpad.alsa@gmail.com</p>
+                <p>+62 822-5229-8589 (Nino)</p>
+                <p>id_line_sekre (Secretariat ID Line) </p>
             </div>
 
-             <div class="footer-social">
+            <div class="footer-social">
                 <h2 class="footer-social-header">Social Media</h2>
     
                 <div class="footer-social-links">
                     <a href="https://www.instagram.com/alsalcunpad/">
                         <i class="fab fa-instagram"></i>
                     </a>
-                    <a href="https://x.com/alsalcunpad">
-                        <i class="fa-brands fa-x-twitter"></i>
-                    </a>
                     <a href="https://www.tiktok.com/@alsalcunpad">
                         <i class="fa-brands fa-tiktok"></i>
+                    </a>
+                    <a href="https://x.com/alsalcunpad">
+                        <i class="fa-brands fa-x-twitter"></i>
                     </a>
                     <a href="https://www.youtube.com/@alsalcunpad2112">
                         <i class="fab fa-youtube"></i>
@@ -31,8 +33,7 @@ const html_footer =   `
         </div>
 
             <div class="footer-group">
-                <a href="../../index.html"><img src="../../asset/logo_alsalcunpad_secondary_color.png" alt="logo ALSA Local Chapter Unpad" class="footer-group-logo"></a>
-
+                <a href="index.html"><img src="./asset/logo_alsalcunpad_secondary_color.png" alt="logo ALSA Local Chapter Unpad" class="footer-group-logo"></a>
     
                 <div class="footer-group-copyright">
                     <p class="footer-group-copyright-p1">©2026 Asian Law Student's Association Local Chapter Unpad</p>
